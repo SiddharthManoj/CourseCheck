5372459487047075dec928986ed6539de6a9fbe4
William Alexander <github.com@willalexander.me>
2014-11-11 03:29:24 +0000

    Update code to server's version

--- loop/loop.php
+++ loop/loop.php
@@ -1,6 +1,7 @@
 <?php
 require('sendgrid-php/sendgrid-php.php');
 
+set_time_limit(0);
 $m = new MongoClient();
 $db = $m->db;
 $sections = $db->sections;
@@ -13,10 +14,18 @@ while (true) {
 		$data = json_decode($contents);
 
 		foreach ($data->OfferedCourses->course as $course) {
-			foreach ($course->CourseData->SectionData as $section) {
-				if (isset($section->id)) {
-					if ($section->id == $targetSection['section']) {
+			$sectionsArray = array();
 
+			if (is_array($course->CourseData->SectionData)) {
+				$sectionsArray = $course->CourseData->SectionData;
+			} else {
+				array_push($sectionsArray, $course->CourseData->SectionData);
+			}
+
+			foreach($sectionsArray as $section) {
+				if (isset($section->id)) {
+					
+					if ($section->id == $targetSection['section']) {
 						$instructorName = property_exists($section, 'instructor') ? $section->instructor->first_name . ' ' . $section->instructor->last_name : '';
 						$info = array('dept' => $data->Dept_Info->abbreviation, 'number' => $course->CourseData->number, 'name' => $course->CourseData->title, 'section' => $section->id, 'prof' => $instructorName, 'start' => $section->start_time, 'end' => $section->end_time, 'seats' => $section->spaces_available, 'taken' => $section->number_registered);
 						$uns = $targetSection['uns'];
@@ -25,14 +34,13 @@ while (true) {
 						$sections->insert($info);
 
 						if ((int) $targetSection['seats'] - (int) $targetSection['taken'] == 0 && (int) $info['seats'] - (int) $info['taken'] > 0) {
-							
 							$sendgrid = new SendGrid('goldani', 'P@ywsE83@q@m5&ykuU5axfs*pn');
 							$email = new SendGrid\Email();
-							$email->setTos($info['uns'])->
-								setFrom('noreply@coursecheck.me')->
-								setFromName('CourseCheck')->
-								setSubject("A course you're watching has new seats available")->
-								setText($info['name'] . ' has new seats available!');
+							$email->setTos($info['uns']);
+							$email->setFrom('noreply@coursecheck.me');
+							$email->setFromName('CourseCheck');
+							$email->setSubject("A course you're watching has new seats available");
+							$email->setText('A course you\'re watching, "' . $info['name'] . '", has new seats available! ');
 							$sendgrid->send($email);
 						}
 					}
@@ -40,7 +48,6 @@ while (true) {
 			}
 		}
 	}
-	echo 'done';
 
 	sleep(300);
 }
